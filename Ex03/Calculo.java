d6b32f2dc347c9a741d6202994bc93ec66b9b778
Isabelli Marques <isabellimarques14@gmail.com>
2024-05-20 11:15:06 -0300

    starting ex 03

--- Ex03/Calculo.java
+++ Ex03/Calculo.java
@@ -1,4 +1,14 @@
 package Ex03;
 
 public class Calculo {
+
+    double percentualAcrescimo;
+    double parcelasEmprestimo;
+
+    public Calculo(double percentualAcrescimo, double parcelasEmprestimo){
+        this.parcelasEmprestimo = 24;
+        this.percentualAcrescimo = 0.35;
+    }
+
+
 }
--- Ex03/Validacao.java
+++ Ex03/Validacao.java
@@ -1,4 +1,14 @@
 package Ex03;
 
 public class Validacao {
+
+    double quantidadeEmprestimo;
+
+    public Validacao(double quantidadeEmprestimo){
+        this.quantidadeEmprestimo = quantidadeEmprestimo;
+    }
+
+    public boolean validaQuantidade(double quantidadeEmprestimo){
+        return quantidadeEmprestimo <= 200000;
+    }
 }
